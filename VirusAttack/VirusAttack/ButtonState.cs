1d0231cba43325920026f88c2d79ee94f88bfdbc
Tomashuk Mikhail <hover024@gmail.com>
2014-04-17 22:55:48 +0300

    Create button class and add button textures

---
+++ VirusAttack/VirusAttack/ButtonState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VirusAttack
+{
+    public enum ButtonState
+    {
+        Normal,
+        Hover,
+        Pressed
+    }
+}
